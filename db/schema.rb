050968a15d8743b62fcd7d46ce24d1d91c06d2c8
Reena Patil <reena.patil@outlook.com>
2017-09-22 00:20:50 +1000

    added gems in gem file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921065157) do
+ActiveRecord::Schema.define(version: 20170921135646) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170921065157) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
